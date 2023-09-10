ed25634be7337bb633ae530560724616094db664
DanyloUserGit <danylom2007.m@gmail.com>
2023-09-10 23:35:46 +0300

    loader

--- index.js
+++ index.js
@@ -18,7 +18,9 @@ app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     next();
   });
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 // app.use(express.static(__dirname + '/media'));
 // app.use(express.static('media')); 
 // app.use('/media', express.static('media'));
